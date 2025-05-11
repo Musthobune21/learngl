f96b4db5a3e455deca09f54700b929674b1226fa
HUA.X.T. <Rolereach2021@163.com>
2025-05-11 09:57:30 +0800

    Learn GL

--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
 CC = gcc
 CFLAGS = -lm -lglfw -lGL -lGLEW -lGLU
 gltest:*.c
-	@$(CC) $^ $(CFLAGS) -o $@
-.PHONY:clean git
+	$(CC) $^ $(CFLAGS) -o $@
+.PHONY:clean git run
 clean:
-	@rm -rf *.out *.o gltest
+	rm -rf *.out *.o gltest
 
 git:
-	@git add .
-	@git commit -m "Learn GL"
-	@git status
+	git add .
+	git commit -m "Learn GL"
+	git status
+run:
+	./gltest
--- gltest.c
+++ gltest.c
@@ -22,8 +22,8 @@ int main(int argc,char *argv[])
 	//shaders
 	GLfloat vertatri[]=
 	{
-		-0.5f,-0.5f,0.0f,1.0f,0.0f,0.0f
-		0.5f,-0.5f,0.0f,0.0f,1.0f,0.0f
+	    -0.5f,-0.5f,0.0f,1.0f,0.0f,0.0f,
+		0.5f,-0.5f,0.0f,0.0f,1.0f,0.0f,
 		0.0f,0.5f,0.0f,0.0f,0.0f,1.0f
 	};
 GLuint VertexID,FragmentID,PID;
@@ -38,7 +38,7 @@ PID=shader_link(VertexID,FragmentID);
 	glBindBuffer(GL_ARRAY_BUFFER,vbo);
 	glBufferData(GL_ARRAY_BUFFER,sizeof vertatri,vertatri,GL_STATIC_DRAW);
 	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(GLfloat), (GLvoid*)0);
-	glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(GLfloat),(GLvoid*)3*sizeof(GLfloat));
+	glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(GLfloat),(GLvoid*)(3*sizeof(GLfloat)));
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
