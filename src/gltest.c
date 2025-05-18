a6ef3014eac873c916fd12e3913861a9175e257e
HUA.X.T. <Rolereach2021@163.com>
2025-05-18 15:06:03 +0800

    Learn GL

--- src/gltest.c
+++ src/gltest.c
@@ -20,10 +20,10 @@ int main(int argc,char *argv[])
 	//shaders
 	GLfloat vertatri[]=
 	{
-	    1.0f,1.0f,0.0f,//0
-	    1.0f,-1.0f,0.0f,//1
-	    -1.0f,-1.0f,0.0f,//2
-	    -1.0f,1.0f,0.0f//3
+	    1.0f,1.0f,0.0f,1.0f,1.0f,//0
+	    1.0f,-1.0f,0.0f,1.0f,-1.0f,//1
+	    -1.0f,-1.0f,0.0f,-1.0f,-1.0f,//2
+	    -1.0f,1.0f,0.0f,-1.0f,1.0f//3
 	};
 	GLuint indwedr[]=
 	{
@@ -51,7 +51,7 @@ PID=shader_link(VertexID,FragmentID);
     load_texture2d("./grass.png",true);
 	glBufferData(GL_ARRAY_BUFFER,sizeof vertatri,vertatri,GL_STATIC_DRAW);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof indwedr,indwedr,GL_STATIC_DRAW);
-	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,5*sizeof(GLfloat), (GLvoid*)0);
 	glVertexAttribPointer(1,2,GL_FLOAT,GL_FALSE,5*sizeof(GLfloat),(GLvoid*)(3*sizeof(GLfloat)));
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
