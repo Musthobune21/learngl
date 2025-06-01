9b90db0eb1ee0b64c48f8f38e3000b95ecb37ec8
HUA.X.T. <Rolereach2021@163.com>
2025-06-01 19:37:49 +0800

    Learn GL

--- src/glsl/vertex.glsl
+++ src/glsl/vertex.glsl
@@ -2,8 +2,9 @@
 layout (location = 0) in vec3 pos;
 layout (location = 1) in vec2 atexcoord;
 out vec2 outcolor;
+uniform mat4 transform;
 void main()
 {
-	gl_Position = vec4(pos.x,pos.y,pos.z,1.0);
+	gl_Position = transform * vec4(pos.x,pos.y,pos.z,1.0f);
 	outcolor = atexcoord;
 }
