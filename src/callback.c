d1436153983a1776df5e03154bc38b04b86133a9
HUA.X.T. <Rolereach2021@163.com>
2025-06-25 18:06:11 +0800

    Learn GL

--- src/callback.c
+++ src/callback.c
@@ -1,5 +1,7 @@
 #include "../include/learngl.h"
 bool keys[1024];
+GLfloat DeltaTime=0.0f;
+GLfloat LastFrame=0.0f;
 void key_callback(GLFWwindow* win,int key,int scancode,int action,int mode)
 {
     if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) glfwSetWindowShouldClose(win, GL_TRUE);
@@ -18,16 +20,19 @@ void framebuffersize_callback(GLFWwindow* win,int width,int height)
 }
 void do_movement()
 {
-    GLfloat speed = 0.05f;
+    GLfloat CurrentFrame=glfwGetTime();
+    DeltaTime=CurrentFrame - LastFrame;
+    LastFrame=CurrentFrame;
+    GLfloat speed = 5.0f * DeltaTime;
     vec3 right;
     glm_cross(cFront,wUp,right);
     glm_normalize(right);
     if(keys[GLFW_KEY_W]) 
-    glm_vec3_muladds(cFront,speed,cPOS);
+        glm_vec3_muladds(cFront,speed,cPOS);
     if(keys[GLFW_KEY_S]) 
-    glm_vec3_mulsubs(cFront,speed,cPOS);
-    if(keys[GLFW_KEY_A]) 
-    glm_vec3_mulsubs(right,speed,cPOS);
-    if(keys[GLFW_KEY_D]) 
-    glm_vec3_muladds(right,speed,cPOS);
+        glm_vec3_mulsubs(cFront,speed,cPOS);
+    if(keys[GLFW_KEY_A])
+        glm_vec3_mulsubs(right,speed,cPOS);
+    if(keys[GLFW_KEY_D])
+        glm_vec3_muladds(right,speed,cPOS);
 }
